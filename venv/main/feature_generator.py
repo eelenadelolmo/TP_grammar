6b3578118f907cdc382d556dcbcba208fe743a97
elenadelolmo <eelena.delolmo@gmail.com>
2020-09-03 04:43:34 +0200

    rewriting dependencies function and searching id function working

--- venv/main/feature_generator.py
+++ venv/main/feature_generator.py
@@ -12,42 +12,65 @@ from itertools import tee, islice, chain
 # Given a string composed of one or several tokens and a sentence
 # Returns the ordered list of ids of the tokens in the sentence
 def search_id(toks, sent):
-    lines = sent.split('\n')
+
+    toks = re.sub('& quot ;', '&quot;', toks)
     toks_list = toks.split(' ')
+    lines = sent.split('\n')
     ids = list()
+
+    # Keeping the ids of every matched line for every token
     for token in toks_list:
         for line in lines:
             if len(line)>1 and token == line.split('\t')[1]:
                 ids.append(line.split('\t')[0])
 
+    # When there are more matched ids than tokens to matchs
     if len(ids) > len(toks_list):
-        matches = 1
+
+        # Creating a new list (by value, not reference) for not losing elements in the iteration for deleting
         ids_copy = ids[:]
+
+        n_matches = 1
+        n_searching = 0
         sig = ""
-        for x in ids_copy:
-            if matches == len(toks_list):
-                until = matches-1
+
+        ids_iter = iter(ids_copy)
+
+        for x in ids_iter:
+
+            # When all tokens have been correctly matched
+            if n_matches == len(toks_list):
+
+                # Keeping the matched tokens
+                until = n_matches-1
                 ids = ids[:until]
                 ids.append(sig)
                 break
+
+            # Calculating and searching the next id to match
             sig = str(int(x) + 1)
+
+            # Deleting the current element if the next one is not in the rest of the list
             if sig not in ids[1:]:
                 ids.remove(x)
+
+            # If the next id to match is in the list
             else:
-                matches += 1
+                # Keeping the elements until the current element and from the next matched id
+                n_x = ids.index(x)
+                n_sig = ids.index(sig)
+                ids = ids[:n_x + 1] + ids[n_sig:]
+                # Skipping the removed elements in the iteration
+                n_to_skip = n_sig - n_x - 1
+                for n in range(n_to_skip):
+                    next(ids_iter)
+
+                n_matches += 1
+
             if len(ids) == len(toks_list):
                 break
 
-    """
-    while len(ids) > len(toks_list):
-        for _, x, next in previous_and_next(ids):
-            if next is None:
-                break
-            elif int(next) < int(x):
-                ids.remove(next)
-            elif int(next) != int(x)+1:
-                ids.remove(x)
-    """
+            n_searching += 1
 
     return ids
 
@@ -67,19 +90,53 @@ def keep_theme_rheme(sent):
     return main
 
 
+# Reindexing the tokens of a sentence to avoid gaps in order to match continuous annotations along them
+def reset_ids(sent):
+    sent_lines = sent.split('\n')
+
+    # Beggining ids from 1
+    id_new = 1
+
+    # Creating a tmp file with both new a original ids
+    tmp = ""
+
+    # Keeping previous and new ids with de format new_id-prev_id
+    for line in sent_lines:
+        if len(line) > 1:
+            tmp += str(id_new) + '-' + line + '\n'
+            id_new += 1
+
+    # Creating the file for the resetted ids
+    resetted = sent
+
+    # Getting the original id
+    for line_tmp in tmp.split('\n'):
+        if len(line_tmp) > 1:
+            id_old = re.search('-(.+)', line_tmp.split('\t')[0]).group(1)
+            id_new = re.search('(.+)-', line_tmp.split('\t')[0]).group(1)
+
+            # Resetted is rewritten for every pair id_old / id_new
+            lines_resetted = resetted.split('\n')
+            resetted = ""
+
+            for line_resetted in lines_resetted:
+                if len(line_resetted) > 1 and line_resetted.split('\t')[6] == id_old:
+                    line_modified = re.sub(id_old + r'(.+?)', str(id_new) + r'\1', line_resetted)
+                elif len(line_resetted) > 1 and line_resetted.split('\t')[0] == id_old:
+                    line_modified = re.sub(r'^' + id_old + '(.+?)', str(id_new) + r'\1', line_resetted)
+                else:
+                    line_modified = line_resetted
+                resetted += line_modified + '\n'
+
+
+    return resetted
+
+
 # Transforms a conllu sentence into the string with its forms
 def txt_transformer(file_conllu):
     s_list = list()
     with open(file_conllu, 'r') as f:
-        lines = f.readlines()
-        ok = ""
-        for l in lines:
-            if len(l) > 1:
-                line_ok = l[:-1] + '\t_\t_\n'
-                ok += line_ok
-            else:
-                ok += l
-    conll = pc.load_from_string(ok)
+        conll = pc.load_from_string(f.read())
     for s in conll:
         s_txt = ""
         for word in s[:-1]:
@@ -87,6 +144,18 @@ def txt_transformer(file_conllu):
         s_txt = s_txt + ".\n"
         s_list.append(s_txt)
     return s_list
+
+
+# Takes the id of the selected head token and the list of ids of its selected dependants and rewrites the sentence dependencies accordingly
+def rewrite_dep(head_id, dep_id, arg, sentence):
+
+    for token in sentence[0]:
+        if token['id'] == int(dep_id):
+            token['head'] = head_id
+            token['deprel'] = arg
+
+    # print(head_id, dep_id, arg, sentence[0].serialize())
+    return sentence[0].serialize()
 
 
 def previous_and_next(some_iterable):
@@ -131,7 +200,7 @@ FN_annotated_list = list()
 
 
 # Getting the ordered list with the FrameNet annotations for every sentence
-files_fm = (os.listdir(dir_FN_annotated))
+files_fm = natsort.natsorted(os.listdir(dir_FN_annotated))
 for file in files_fm:
     with open(dir_FN_annotated + '/' + file, 'r') as f:
         dict_ann = literal_eval(f.read())
@@ -143,25 +212,63 @@ n_sentence = 0
 with open(dir_TP_annotated + '/' + file_grew) as f:
     sentences = parse(f.read())
 
+    # --> String representation of the conllu structure of the sentences in a text
     fm_annotated = ""
+
     for sentence in sentences:
 
+        # Reducing every sentence to the theme + rheme of the main clause
         sentence_main = keep_theme_rheme(sentence.serialize())
+
+        # Rewriting the ids: beginning from 1 and with no gaps
+        sentence_main = reset_ids(sentence_main)
+
         fm_anns = FN_annotated_list[n_sentence]
         n_sentence += 1
 
         # Getting the ids of the tokens conforming the head of a FrameNet frame
-        for ann_head in fm_anns:
-            ann = ann_head[0]
-            head = ann_head[1]
-            h_id = search_id(head, sentence.serialize())
+        for frame_head in fm_anns:
 
-        fm_annotated += sentence_main + '\n'
+            # --> FrameNet frame
+            frame = frame_head[0]
+
+            # --> Tokens representing the head of the arguments
+            frame_tokens = frame_head[1]
+
+            # --> List of the ids (ordered) of the tokens corresponding to the head of the arguments
+            h_ids = search_id(frame_tokens, sentence_main)
+
+            for dep_ann in fm_anns[frame_head]:
+
+                # --> The type of argument
+                argument_type = dep_ann[0]
+
+                # --> The tokens representing the argument
+                argument_tokens = dep_ann[1]
+
+                # --> List of the ids (ordered) of the tokens correponding to the argument
+                h_dep_ids = search_id(argument_tokens, sentence_main)
+                # print(argument_tokens, h_dep_ids, '\n' + sentence_main)
+
+                # Making the tokens of a dependency depend on its first token
+                for h_dep_id in h_dep_ids[1:]:
+                    sentence_main = rewrite_dep(h_dep_ids[0], h_dep_id, argument_type, parse(sentence_main))
 
 
-    # Creating the HTML file in the template folder
+        fm_annotated += (sentence_main + '\n')
+        print(fm_annotated)
+
+    # Creating a new file with the selected subtree with the FrameNet annotations
     with open(dir_output + '/' + file_grew, "w") as h:
-        h.write(fm_annotated)
+        lines = fm_annotated.split('\n')
+        ok = ""
+        for l in lines:
+            if len(l) > 1:
+                line_ok = l[:-1] + '\t_\t_\n'
+                ok += line_ok
+            else:
+                ok += (l + '\n')
+        h.write(ok)
         h.close()
 
 
